a8e7adf412012db23288f58ac298b9f8a88777ec
s21023 <s21023@std.it-college.ac.jp>
2023-08-22 05:24:07 +0000

    44修正

--- 44/44-003.php
+++ 44/44-003.php
@@ -1,19 +1,17 @@
 <?php
   session_start();
   header('Content-Type: text/html; charset=UTF-8');
-  $id = @$_POST['ID'];   // ユーザID
-  $pwd = @$_POST['PWD']; // パスワード
-  // データベースに接続
+  $id = @$_POST['ID'];   
+  $pwd = @$_POST['PWD']; 
   $dbhost = $_ENV['MYSQL_HOST'] ?? '127.0.0.1';
   $db = new PDO("mysql:host=$dbhost;dbname=wasbook", "root", "wasbook");
-  // SQLの組み立て
   $sql = "SELECT * FROM users WHERE id ='$id' AND PWD = '$pwd'";
-  $ps = $db->query($sql);   // クエリー実行
+  $ps = $db->query($sql);   
 ?>
 <html>
 <body>
 <?php
-  if ($ps->rowCount() > 0) { // SELECTした行が存在する場合ログイン成功
+  if ($ps->rowCount() > 0) { 
     $_SESSION['id'] = $id;
     echo 'ログイン成功です';
   } else {
